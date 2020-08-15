766e1e85625dac9c6d9e699b0eb073ec6e7ce567
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-15 17:22:36 -0500

    intial commit

---
+++ covid-19-tracker/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a COVID 19 tracker!</h1>
+    </div>
+  );
+}
+
+export default App;
