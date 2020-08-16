b4f08c96a5a3af0533256a3277d381a9e9dad66b
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-16 17:48:23 -0500

    preparing to code an API

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+ const [countries, setCountries] = useState([
+     'USA', 'UK', 'INDIA'
+ ]);
+
+ // STATE = How to write a variable in REACT <<<<<
+
+ // https://disease.sh/v3/covid-19/countires
+
+ // USEEFFECT = Runs a piece of code based on a given condition
+
+ useEffect(() => {
+   // The code inside here will run once when the component loads and not again
+ }, [countries]);
+
   return (
     <div className="app">
-      <h1>Lets build a COVID 19 tracker!</h1>
+        <div className="app_header">
+            <h1>COVID-19 TRACKER</h1>
+            <FormControl classname="app_dropdown">
+                <Select variant="outlined" value="abc">
+                    {/* Loop through all the countries and show a drop down list of the options */}
+
+                    {
+                        countries.map(country => (
+                        <MenuItem value="country">{country}</MenuItem>
+                        ))
+                    }
+
+                    {/*<MenuItem value="worldwide">Worldwide</MenuItem>*/}
+                    {/*<MenuItem value="worldwide">Option 2</MenuItem>*/}
+                    {/*<MenuItem value="worldwide">Option 3</MenuItem>*/}
+                    {/*<MenuItem value="worldwide">Yo</MenuItem>*/}
+                </Select>
+            </FormControl>
+        </div>
+
 
         {/* Header */}
         {/* Title + Select input dropdown field*/}
