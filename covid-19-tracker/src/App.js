c53628802394207070ee6b1412f96c97f1c1f613
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-16 18:12:48 -0500

    Made an API request, pulled , then populated select list in react

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -3,19 +3,24 @@ import { MenuItem, FormControl, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
- const [countries, setCountries] = useState([
-     'USA', 'UK', 'INDIA'
- ]);
-
- // STATE = How to write a variable in REACT <<<<<
-
- // https://disease.sh/v3/covid-19/countires
-
- // USEEFFECT = Runs a piece of code based on a given condition
+ const [countries, setCountries] = useState([]);
 
  useEffect(() => {
-   // The code inside here will run once when the component loads and not again
- }, [countries]);
+     const getCountriesData = async () => {
+         await fetch("https://disease.sh/v3/covid-19/countries")
+             .then((response) => response.json())
+             .then((data) => {
+                 const countries = data.map((country) => ({
+                         name: country.country, // United States, United Kingdom, France
+                         value: country.countryInfo.iso2, // UK , USA , FR
+                     }));
+
+                 setCountries(countries);
+             });
+     };
+
+     getCountriesData();
+ }, []);
 
   return (
     <div className="app">
@@ -25,16 +30,9 @@ function App() {
                 <Select variant="outlined" value="abc">
                     {/* Loop through all the countries and show a drop down list of the options */}
 
-                    {
-                        countries.map(country => (
-                        <MenuItem value="country">{country}</MenuItem>
-                        ))
-                    }
-
-                    {/*<MenuItem value="worldwide">Worldwide</MenuItem>*/}
-                    {/*<MenuItem value="worldwide">Option 2</MenuItem>*/}
-                    {/*<MenuItem value="worldwide">Option 3</MenuItem>*/}
-                    {/*<MenuItem value="worldwide">Yo</MenuItem>*/}
+                    {countries.map(country => (
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                        ))}
                 </Select>
             </FormControl>
         </div>
